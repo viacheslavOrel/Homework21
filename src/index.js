80e2fd9a2e72d4a37e79d2fe2ad5fd52a2bc5d19
Viacheslav Orel <trosha2306@gmail.com>
2020-03-22 17:08:39 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,59 @@
+function getKeys(obj) {
+    const arr = [];
+
+    for(const key in obj) {
+        arr.push(key);
+    }
+
+    return arr;
+}
+
+function getValues(obj) {
+    const arr = [];
+
+    for(const key in obj) {
+        arr.push(obj[key]);
+    }
+
+    return arr;
+}
+
+function getEntries(obj) {
+    const arr = [];
+
+    for(const key in obj) {
+        arr.push([key, obj[key]]);
+    }
+
+    return arr;
+}
+
+const testObj = {
+    name: 'Brendan',
+    surname: 'Eich',
+    byear: 1961,
+    company: 'Netscape'
+};
+
+console.log('Object.keys: ', Object.keys(testObj));
+console.log('getKeys: ', getKeys(testObj));
+
+console.log('Object.values: ', Object.values(testObj));
+console.log('getValues: ', getValues(testObj));
+
+console.log('Object.entries: ', Object.entries(testObj));
+console.log('getEntries: ', getEntries(testObj));
+
+function getX (y) {
+    const findX = (startValue, endValue) => {
+        const middleValue = Math.round((endValue - startValue) / 2) + startValue;
+        const condition = middleValue ** 2 + middleValue;
+
+        if (condition === y) return middleValue;
+        else return condition < y ? findX(middleValue, endValue) : findX(startValue, middleValue);
+    };
+
+    return `x**2 + x = ${y};\nx = ${findX(0, Math.round(Math.sqrt(y)))}`;
+}
+
+console.log(getX(110));
